19d6ec375cba1984b0104e8355027bb129a2dc18
Kruutus <kel.kris94@gmail.com>
2020-10-02 08:59:22 +0200

    Updates

---
+++ assets/OfferBlogLogo.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+
+const OfferBlogLogo: React.FC = () => (
+  <svg
+    width="35"
+    height="35"
+    viewBox="0 0 35 35"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M25.34 28H9.65997C9.53311 28 9.41145 27.9496 9.32174 27.8599C9.23204 27.7702 9.18164 27.6485 9.18164 27.5217V10.1733L12.3666 7H25.34C25.4028 7 25.465 7.01237 25.523 7.03641C25.5811 7.06045 25.6338 7.09568 25.6782 7.1401C25.7226 7.18452 25.7579 7.23725 25.7819 7.29528C25.8059 7.35332 25.8183 7.41552 25.8183 7.47833V27.5333C25.8153 27.6582 25.7635 27.7769 25.6741 27.864C25.5848 27.9512 25.4648 28 25.34 28V28Z"
+      stroke="#FFC800"
+      strokeMiterlimit="10"
+    />
+    <path
+      d="M11.4683 15.4001H17.6166"
+      stroke="#FFC800"
+      strokeMiterlimit="10"
+      strokeLinecap="round"
+    />
+    <path
+      d="M11.4683 17.8616H22.0733"
+      stroke="#FFC800"
+      strokeMiterlimit="10"
+      strokeLinecap="round"
+    />
+    <path
+      d="M11.4683 20.1833H17.6166"
+      stroke="#FFC800"
+      strokeMiterlimit="10"
+      strokeLinecap="round"
+    />
+    <path
+      d="M12.3666 7V9.695C12.3666 9.82186 12.3162 9.94353 12.2265 10.0332C12.1368 10.1229 12.0152 10.1733 11.8883 10.1733H9.18164L12.3666 7Z"
+      stroke="#FFC800"
+      strokeLinejoin="bevel"
+    />
+  </svg>
+);
+
+export default OfferBlogLogo;
---
+++ assets/OfferVideoLogo.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const OfferVideoLogo: React.FC = () => (
+  <svg
+    width="35"
+    height="35"
+    viewBox="0 0 35 35"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M29.1667 9.05322H5.8217C5.17736 9.05322 4.65503 9.57556 4.65503 10.2199V24.7916C4.65503 25.4359 5.17736 25.9582 5.8217 25.9582H29.1667C29.811 25.9582 30.3334 25.4359 30.3334 24.7916V10.2199C30.3334 9.57556 29.811 9.05322 29.1667 9.05322Z"
+      stroke="#FFC800"
+      strokeMiterlimit="10"
+    />
+    <path
+      d="M17.4999 22.5167C20.2706 22.5167 22.5166 20.2707 22.5166 17.5001C22.5166 14.7294 20.2706 12.4834 17.4999 12.4834C14.7293 12.4834 12.4833 14.7294 12.4833 17.5001C12.4833 20.2707 14.7293 22.5167 17.4999 22.5167Z"
+      stroke="#FFC800"
+      strokeMiterlimit="10"
+    />
+    <path
+      d="M16.8234 15.5398L19.2034 17.7215L16.8234 19.9031V15.5398Z"
+      stroke="#FFC800"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+);
+
+export default OfferVideoLogo;
