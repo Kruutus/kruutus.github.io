ea766bf8a4175af3daaaeca4b4da1a8c64c1427a
Kruutus <kel.kris94@gmail.com>
2020-11-11 21:01:19 +0100

    Updates

---
+++ assets/icons.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+// eslint-disable-next-line import/prefer-default-export
+export const PlayIcon = () => (
+  <svg width="54" height="54" viewBox="0 0 54 54" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <g filter="url(#filter0_d)">
+      <circle cx="27" cy="23" r="20" fill="#FFC800" />
+      <path d="M32.5125 24.2046L24.7172 28.7518C24.0506 29.1407 23.2134 28.6598 23.2134 27.888V18.7935C23.2134 18.0217 24.0506 17.5409 24.7173 17.9297L32.5125 22.477C33.174 22.8629 33.174 23.8187 32.5125 24.2046Z" fill="white" />
+    </g>
+    <defs>
+      <filter id="filter0_d" x="0" y="0" width="54" height="54" filterUnits="userSpaceOnUse" colorInterpolationFilters="sRGB">
+        <feFlood floodOpacity="0" result="BackgroundImageFix" />
+        <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" />
+        <feOffset dy="4" />
+        <feGaussianBlur stdDeviation="3.5" />
+        <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.15 0" />
+        <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow" />
+        <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow" result="shape" />
+      </filter>
+    </defs>
+  </svg>
+);
